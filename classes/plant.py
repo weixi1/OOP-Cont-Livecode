1109069707403ae1c92a7c66384faa74819ae0ae
Weixi He <heweixiguge@gmai.com>
2024-10-01 14:09:12 -0400

    test_plant

--- classes/plant.py
+++ classes/plant.py
@@ -1,4 +1,33 @@
 class Plant:
 
 # Write your plant class here!
-    pass
+    def __init__(self, name, water_level, sunlight_hours, is_blooming=False):
+        self.name = name
+        self.water_level = water_level
+        self.sunlight_hours = sunlight_hours
+        self.is_blooming = is_blooming
+
+    def water(self, water_amout):
+        self.water_level += water_amout
+
+        return f"{self.name} new water level: {self.water_level}"
+    
+    def give_sunlight(self, hours):
+        self.sunlight_hours += hours
+
+        return f"{self.name} new sunlight hours: {self.sunlight_hours}"
+    
+    def check_growth(self):
+        if self.sunlight_hours > 5 and self.water_level > 5:
+            return "mature"
+            # self.growth = "mature"
+        else:
+            return "sprout"
+            # self.growth = "sprout"
+    
+plant = Plant("Butterfly pea floewr" , 6, 7)   
+
+print(plant.check_growth())
+    
+
+    
--- tests/test_plant.py
+++ tests/test_plant.py
@@ -1,1 +1,21 @@
 from classes.plant import Plant
+
+def test_plant_initalization():
+    # Arrange
+    name = "Butterfly pea floewr"
+    water_level = 6
+    sunlight_hours = 7
+    is_blooming = True
+
+    # Act
+    plant = Plant(name, water_level, sunlight_hours, is_blooming)
+
+    # assert
+    assert plant.name == name
+    assert plant.is_blooming
+
+    # assert not plant_2.is_blooming
+
+
+
+
